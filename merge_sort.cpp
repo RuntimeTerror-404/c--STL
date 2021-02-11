f8260282d0b03f2832b1e043c4830c0ab5c81931
Mohit Parashar <mohit1672002@gmail.com>
2021-02-11 23:32:25 +0530

    second changes

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,35 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int partition(int *a, int s,int e){
-    int i = s-1;
-    int j = s;
-    int pivot = a[e];
-    for( ;j<e; j++){
-        if(a[j] <= pivot){
-            i++;
-            swap(a[i], a[j]);
-        }
-    }
-    swap(a[i+1],  a[e]);
-    return i+1;
-};
 
-void quickSort(int *a, int s, int e){
-    if(s>=e){
-        return;
-    }
-
-    int p = partition(a, s, e);
-    quickSort(a, s, p-1);
-    quickSort(a, p+1, e);
-}
 
 int main(){
-    int a[] = {2, 1, 4, 6, 7, 5, 8};
-    int n = sizeof(a)/sizeof(int);
-    quickSort(a, 0, n-1);
-
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    sorting(a[]);
     for(int i=0; i<n; i++){
         cout<<a[i]<<" ";
     }
